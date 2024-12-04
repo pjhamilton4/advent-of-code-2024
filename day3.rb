fd63cd1c9717425d9b4302dffb318fc901c0433a
Philip Hamilton <pjhamilton4@gmail.com>
2024-12-03 23:45:24 -0500

    day 3 broken second part

--- day3.rb
+++ day3.rb
@@ -4,14 +4,19 @@ end
 def main
   input = read_file("./inputs/day3.txt")
   #puts "Input: #{input}"
-  matches = input.scan(/mul[(]\d{1,3},\d{1,3}[)]/)
+  with_exclusions = input.scan(/mul[(]\d{1,3},\d{1,3}[)]/)
+  removed_exclusions = input.gsub(/[don't()]{1}.*(mul[(]\d{1,3},\d{1,3}[)])*.*[do()]{1}/, '')
+  matches = removed_exclusions.scan(/mul[(]\d{1,3},\d{1,3}[)]/)
+  puts "With Exclusions: #{with_exclusions}"
   puts "Matches: #{matches}"
   total = 0
-  for match in matches
-    left = match.scan(/\d+/).first.to_i
-    right = match.scan(/\d+/).last.to_i
-    total += (right * left)
-  end
+  matches.each { |match|
+    if match != nil
+      left = match.scan(/\d+/).first.to_i
+      right = match.scan(/\d+/).last.to_i
+      total += (right * left)
+    end
+  }
   total
 end
 
